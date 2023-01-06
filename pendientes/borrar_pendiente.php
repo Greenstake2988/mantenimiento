89aaa23da08eef33a533d54484b12c0e29c9ac58
Oscar Vazquez <0skr.vazquez@gmail.com>
2023-01-06 16:46:40 -0600

    test

--- pendientes/borrar_pendiente.php
+++ pendientes/borrar_pendiente.php
@@ -31,8 +31,7 @@ include("../../../conexion/enchufarsolicitudes.php");
 //actualizar para centro de computo
 if($_SESSION['sol']=='cc')
             {
-            $pendiente = 0;
-            $modificar="Update solicitudes Set pendiente='$pendiente' where folio='$folio2' and anio_folio='$anio' and solicita_a='cc'";
+            $modificar="Update solicitudes Set pendiente=0 where folio='$folio2' and anio_folio='$anio' and solicita_a='cc'";
             $borrar="Delete FROM orden_mantenimiento WHERE where folio='$folio2' and anio_folio='$anio'";
             $resultado=mysql_query($modificar,$enchufarsolicitudes);
             $resultado2=mysql_query($borrar,$enchufarsolicitudes);
